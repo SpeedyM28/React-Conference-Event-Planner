80678046edd3ac20627358137e4faaa60d9bb299
SpeedyM28 <jamil.bitarm76@gmail.com>
2025-01-14 07:33:54 -0500

    Task 4:21

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -31,9 +31,15 @@ const ConferenceEvent = () => {
         }
       };
     const handleIncrementAvQuantity = (index) => {
+        if(avItems[index]){
+            dispatch(incrementAvQuantity(index));
+        }
     };
 
     const handleDecrementAvQuantity = (index) => {
+        if(avItems[index]){
+            dispatch(decrementAvQuantity(index));
+        }
     };
 
     const handleMealSelection = (index) => {
@@ -159,7 +165,20 @@ const ConferenceEvent = () => {
 
                                 </div>
                                 <div className="addons_selection">
-
+                                    {avItems.map((item,index) => (
+                                        <div className="av_data venue_main" key={index}>
+                                            <div className="img">
+                                                <img src={item.img} alt={item.name} />
+                                                </div>
+                                            <div className="text"> {item.name} </div>
+                                            <div> ${item.cost} </div>
+                                                <div className="addons_btn">
+                                                    <button className="btn-warning" onClick={() => handleDecrementAvQuantity(index)}> &ndash; </button>
+                                                    <span className="quantity-value">{item.quantity}</span>
+                                                    <button className="btn-success" onClick={() => handleIncrementAvQuantity(index)}> &#43; </button>
+                                                </div>
+                                            </div>        
+                                    ))}
                                 </div>
                                 <div className="total_cost">Total Cost:</div>
 
